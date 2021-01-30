2d44785a1c96c3dffb96758cc63cca15d78ae397
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-01-30 22:42:44 +0700

    First version of calculator

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+document.querySelector('.btn').addEventListener('click',function(){
+	var aod = document.getElementById("Input").value;
+	if(aod <= 1 && aod >=0){
+		const eff = aod * 48.0;
+		const rev = eff * 1000 * 40000;
+		document.querySelector(".efficiency").textContent = `The efficiency is ${eff} with ${aod * 100}% AOD  `;
+		document.querySelector(".revenue").textContent = `The loss is ${rev} USD (Measure in 100 gigawatts) `;
+	}else{
+		document.querySelector(".efficiency").textContent = `Invalid data`;
+		document.querySelector(".revenue").textContent = ``;
+	}
+});
+
+
