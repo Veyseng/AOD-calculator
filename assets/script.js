b17492a604f8e8a18b413553483884a6880aa237
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-01-30 23:02:52 +0700

    second version

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ document.querySelector('.btn').addEventListener('click',function(){
 	if(aod <= 1 && aod >=0){
 		const eff = aod * 48.0;
 		const rev = eff * 1000 * 40000;
-		document.querySelector(".efficiency").textContent = `The efficiency is ${eff} with ${aod * 100}% AOD  `;
+		document.querySelector(".efficiency").textContent = `The efficiency loss is ${eff}% with ${aod} AOD  `;
 		document.querySelector(".revenue").textContent = `The loss is ${rev} USD (Measure in 100 gigawatts) `;
 	}else{
 		document.querySelector(".efficiency").textContent = `Invalid data`;
