904cd06b1f9975e2f984f5d35ec8d857f7c21b01
Veyseng Choeng <choeng.veyseng1@gmail.com>
2021-01-31 00:09:08 +0700

    Edit some texts

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,17 @@
 document.querySelector('.btn').addEventListener('click',function(){
 	var aod = document.getElementById("Input").value;
 	if(aod <= 1 && aod >=0){
-		const eff = aod * 48.0;
+		const eff = aod * 48;
 		const rev = eff * 1000 * 40000;
-		document.querySelector(".efficiency").textContent = `The efficiency loss is ${eff}% with ${aod} AOD  `;
-		document.querySelector(".revenue").textContent = `The loss is ${rev} USD (Measure in 100 gigawatts) `;
+		document.querySelector(".progress").style.visibility = "visible";
+		document.querySelector(".progress").value = eff;
+		document.querySelector(".efficiency").textContent = `The Loss of Efficiency is ${eff}% with ${aod} AOD value`;
+		document.querySelector(".revenue").textContent = `The Loss of Revenue is ${rev} US Dollars / Year`;	
+		
 	}else{
 		document.querySelector(".efficiency").textContent = `Invalid data`;
 		document.querySelector(".revenue").textContent = ``;
+		document.querySelector(".progress").style.visibility = "hidden";
 	}
 });
 
